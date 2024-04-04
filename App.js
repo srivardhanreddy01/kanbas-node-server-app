831b80a78247c1a3dcf0a34e655c6698e9f25c1e
srivardhanreddy01 <srivardhanreddy01@gmail.com>
2024-04-03 22:02:28 -0400

    Changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import Lab5 from "./Lab5.js";
 import cors from "cors";
 import express from 'express';
 import CourseRoutes from "./Kanbas/courses/routes.js";
-import ModuleRoutes from "./Kanbas/Modules/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentsRoutes from "./Kanbas/assignments/routes.js";
 
 const app = express()
